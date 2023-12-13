793de9c27504bf6e34591244547877b45d049b82
NikolayKuraga <kolya54686579@yandex.ru>
2023-12-13 22:51:24 +0300

    added wide tabs to main.c

--- task1/main.c
+++ task1/main.c
@@ -4,65 +4,92 @@
 #include "utils.h"
 #include "parallel.h"
 
-int main(int argc, char** argv) {
-    double* matrix;
-    double* vec;
-    double* product;
-    int rank;
-    int size;
-    int matrixRows;
-    int matrixCols;
-    char fileNameMatrix[30];
-    char fileNameVec[30];
-    char fileNameResult[40];
-    double start = 0;
-    double finish = 0;
-    double elapsed = 0;
-    double time_elapsed = 0;
-    int dim = strtol(argv[1], NULL, 10);
 
-    MPI_Init(&argc, &argv); 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+#define FILE_NAME_LEN 128
 
-    matrixRows = dim;
-    matrixCols = dim;
-    matrix = (double*) malloc(matrixRows * matrixCols * sizeof(double));
-    vec = (double*) malloc(matrixCols * sizeof(double));
-    product = (double*) malloc(matrixCols * sizeof(double));
 
-    if (rank == 0){
-        sprintf(fileNameMatrix, "input/matrix-%d-%d.txt", matrixRows, matrixCols);
-        readArray(fileNameMatrix, matrix, matrixRows * matrixCols);
+int main(int argc, char** argv)
+{
+	int lclTmp;
 
-        sprintf(fileNameVec, "input/vec-%d.txt", matrixCols);
-        readArray(fileNameVec, vec, matrixCols);
-    }
+	double* p_mat = NULL;
+	double* p_vec = NULL;
+	double* p_rslt = NULL;
 
-    MPI_Barrier(MPI_COMM_WORLD);
-    start = MPI_Wtime();
-    parallelProduct(matrix, vec, product, matrixRows, matrixCols, rank, size);
-    finish = MPI_Wtime();
-    MPI_Barrier(MPI_COMM_WORLD);
+	double timeStrt = 0.;
+	double timeFnsh = 0.;
+	double timeElapsed = 0.;
 
-    time_elapsed = finish - start;
-    MPI_Reduce(&time_elapsed, &elapsed, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+	int dim = strtol(argv[1], NULL, 10);
+	int matRowsN = dim;
+	int matClmsN = dim;
 
-    if (rank == 0){
-        sprintf(fileNameResult, "output/parallel-product-%d-%d.txt", matrixRows, matrixCols);
-        writeArray(fileNameResult, product, matrixRows);
-        printf(
-            "Time (dim=%d, n_threads=%d): %lf\n",
-            dim,
-            size,
-            time_elapsed
-        );
-    }
+	int rank;
+	int size;
 
-    free(matrix);
-    free(vec);
-    free(product);
+	MPI_Init(&argc, &argv); 
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    MPI_Finalize();
-    return 0;
+	p_vec = malloc(matClmsN * sizeof(*p_vec));
+
+	if (rank == 0) {
+		char fnameMat[FILE_NAME_LEN] = {0};
+		char fnameVec[FILE_NAME_LEN] = {0};
+
+		p_mat = malloc(matRowsN * matClmsN * sizeof(*p_mat));
+		p_rslt = malloc(matClmsN * sizeof(*p_rslt));
+
+		sprintf(
+			fnameMat,
+			"input/mat-%d-%d.txt",
+			matRowsN,
+			matClmsN);
+
+		if (readArray(fnameMat, p_mat, matRowsN * matClmsN)) {
+			puts("KU! INPUT MATRIX FILENAME ERROR!");
+			MPI_Abort(MPI_COMM_WORLD, 1);
+		}
+
+		sprintf(fnameVec, "input/vec-%d.txt", matClmsN);
+		if (readArray(fnameVec, p_vec, matClmsN)) {
+			puts("KU! INPUT VECTOR FILENAME ERROR!");
+			MPI_Abort(MPI_COMM_WORLD, 1);
+		}
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	if (rank == 0) { timeStrt = MPI_Wtime(); }
+	parallelProduct(p_mat, p_vec, p_rslt, matRowsN, matClmsN, rank, size);
+	MPI_Barrier(MPI_COMM_WORLD);
+	if (rank == 0) {
+		timeFnsh = MPI_Wtime();
+		timeElapsed = timeFnsh - timeStrt;
+	}
+
+	if (rank == 0) {
+		char fnameRslt[FILE_NAME_LEN] = {0};
+
+		sprintf(
+			fnameRslt,
+			"output/parallel-p_rslt-%d-%d.txt",
+			matRowsN,
+			matClmsN);
+
+		if (writeArray(fnameRslt, p_rslt, matRowsN)) {
+			puts("KU! OUTPUT FILENAME ERROR!");
+			MPI_Abort(MPI_COMM_WORLD, 1);
+		}
+		printf(
+			"Time (rows_n=%d, clms_n=%d, threads=%d): %lf\n",
+			matRowsN, matClmsN, size, timeElapsed);
+
+		free(p_mat);
+		free(p_rslt);
+	}
+
+	free(p_vec);
+
+	MPI_Finalize();
+	return 0;
 }
--- task1/utils.c
+++ task1/utils.c
@@ -2,22 +2,27 @@
 #include <stdlib.h>
 #include "utils.h"
 
-void readArray(char* fileName, double* array, int n) {
+int readArray(char* fileName, double* array, int n) {
     int i;
     FILE* fp = fopen(fileName, "r");
+    if (fp == NULL) { return 1; }
+
     for (i = 0; i < n; ++i){
         fscanf(fp, "%lf", &array[i]);
     }
     fclose(fp);
+    return 0;
 }
 
-void writeArray(char* fileName, double* array, int n) {
-    FILE *fp = fopen(fileName, "w+");
-    
+int writeArray(char* fileName, double* array, int n) {
     int i;
+    FILE *fp = fopen(fileName, "w+");
+    if (fp == NULL) { return 1; }
+
     for (i = 0; i < n; ++i) {
         fprintf(fp, "%.6f\n", array[i]);
     }
 
     fclose(fp);
+    return 0;
 }
