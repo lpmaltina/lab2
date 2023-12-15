c78e59f04417a3e45ac4cbe48409666eb1efabf8
Cath224 <yakovkate24@gmail.com>
2023-12-16 01:49:49 +0300

    fix block

--- task1/parallel-product-blocks.c
+++ task1/parallel-product-blocks.c
@@ -2,48 +2,73 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-void parallelProduct(double* matrix, double* vector, double* result, int rows, int cols, int rank, int size) {
-    int blockSize = rows / size;
-    int remainingRows = rows % size;
-    int myRows = rank < remainingRows ? blockSize + 1 : blockSize;
+void parallelProduct(double* matrix, double* vector, double* result, int rows, int cols, int rank, int size, int blockSize) {
+    int i;
+    int j;
+    int b;
+    int myStartBlock, myEndBlock;
+    int numBlocks = cols / blockSize;
+    if (cols % blockSize != 0) {
+        numBlocks++;
+    }
 
-    double* myMatrix = (double*)malloc(myRows * cols * sizeof(double));
-    double* myResult = (double*)malloc(myRows * sizeof(double));
+    myStartBlock = rank * numBlocks / size;
+    myEndBlock = (rank + 1) * numBlocks / size;
+    if (rank == size - 1) {
+        myEndBlock = numBlocks;
+    }
 
-    int* sendCounts = (int*)malloc(size * sizeof(int));
-    int* displacements = (int*)malloc(size * sizeof(int));
+    double* myMatrixBlock = (double*)malloc(rows * blockSize * sizeof(double));
+    double* myVectorBlock = (double*)malloc(blockSize * sizeof(double));
 
-    if (rank == 0) {
-        int offset = 0;
-        for (int i = 0; i < size; i++) {
-            sendCounts[i] = (i < remainingRows ? blockSize + 1 : blockSize) * cols;
-            displacements[i] = offset;
-            offset += sendCounts[i];
+    for (int b = myStartBlock; b < myEndBlock; b++) {
+        int myBlockOffset = b * blockSize;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < blockSize; j++) {
+                int matrixIndex = i * cols + myBlockOffset + j;
+                if (matrixIndex < rows * cols) {
+                    myMatrixBlock[i * blockSize + j] = matrix[matrixIndex];
+                } else {
+                    myMatrixBlock[i * blockSize + j] = 0.0;
+                }
+            }
+        }
+
+        for (int i = 0; i < blockSize; i++) {
+            int vectorIndex = myBlockOffset + i;
+            if (vectorIndex < cols) {
+                myVectorBlock[i] = vector[vectorIndex];
+            } else {
+                myVectorBlock[i] = 0.0;
+            }
         }
     }
 
-    MPI_Scatterv(matrix, sendCounts, displacements, MPI_DOUBLE, myMatrix, myRows * cols, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    MPI_Bcast(vector, cols, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    for (int i = 0; i < myRows; ++i) {
-        myResult[i] = 0.0;
-        for (int j = 0; j < cols; ++j) {
-            myResult[i] += myMatrix[i * cols + j] * vector[j];
+    double* myResultBlock = (double*)malloc(rows * sizeof(double));
+    for (int i = 0; i < rows; i++) {
+        myResultBlock[i] = 0.0;
+        for (int j = 0; j < blockSize; j++) {
+            myResultBlock[i] += myMatrixBlock[i * blockSize + j] * myVectorBlock[j];
         }
     }
 
-    if (rank == 0) {
-        for (int i = 0; i < size; i++) {
-            sendCounts[i] = i < remainingRows ? blockSize + 1 : blockSize;
-            displacements[i] = (i > 0) ? displacements[i - 1] + sendCounts[i - 1] : 0;
+    int* recvcounts = (int*)malloc(size * sizeof(int));
+    int* displs = (int*)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) {
+        int startBlock = i * numBlocks / size;
+        int endBlock = (i + 1) * numBlocks / size;
+        if (i == size - 1) {
+            endBlock = numBlocks;
         }
+        recvcounts[i] = rows * (endBlock - startBlock);
+        displs[i] = rows * startBlock;
     }
 
-    MPI_Gatherv(myResult, myRows, MPI_DOUBLE, result, sendCounts, displacements, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Gatherv(myResultBlock, rows, MPI_DOUBLE, result, recvcounts, displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    free(myMatrix);
-    free(myResult);
-    free(sendCounts);
-    free(displacements);
+    free(myMatrixBlock);
+    free(myVectorBlock);
+    free(myResultBlock);
+    free(recvcounts);
+    free(displs);
 }
