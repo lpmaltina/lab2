95a91b45241f96c3f9a2f286db07e8745d7c2467
Cath224 <yakovkate24@gmail.com>
2023-12-17 14:26:14 +0300

    fix block

--- task1/parallel-product-blocks.c
+++ task1/parallel-product-blocks.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-void parallelProduct(double* matrix, double* vector, double* result, int rows, int cols, int rank, int size, int blockSize) {
-    int i;
-    int j;
-    int b;
+void parallelProductBlock(double* matrix, double* vector, double* result, int rows, int cols, int rank, int size, int blockSize) {
+    int i, j, b;
     int myStartBlock, myEndBlock;
     int numBlocks = cols / blockSize;
     if (cols % blockSize != 0) {
@@ -18,13 +16,18 @@ void parallelProduct(double* matrix, double* vector, double* result, int rows, i
         myEndBlock = numBlocks;
     }
 
-    double* myMatrixBlock = (double*)malloc(rows * blockSize * sizeof(double));
-    double* myVectorBlock = (double*)malloc(blockSize * sizeof(double));
+    int blockMatrixSize = rows * blockSize;
 
-    for (int b = myStartBlock; b < myEndBlock; b++) {
+    double* myMatrixBlock = (double*)malloc(blockMatrixSize * sizeof(double));
+    double* myVectorBlock = (double*)malloc(blockSize * sizeof(double));
+    MPI_Bcast(vector, cols, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    for (b = myStartBlock; b < myEndBlock; b++) {
         int myBlockOffset = b * blockSize;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < blockSize; j++) {
+        MPI_Scatter(matrix + myBlockOffset, blockSize, MPI_DOUBLE, myMatrixBlock, blockSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        for (i = 0; i < rows; i++) {
+            for (j = 0; j < blockSize; j++) {
                 int matrixIndex = i * cols + myBlockOffset + j;
                 if (matrixIndex < rows * cols) {
                     myMatrixBlock[i * blockSize + j] = matrix[matrixIndex];
@@ -34,7 +37,7 @@ void parallelProduct(double* matrix, double* vector, double* result, int rows, i
             }
         }
 
-        for (int i = 0; i < blockSize; i++) {
+        for (i = 0; i < blockSize; i++) {
             int vectorIndex = myBlockOffset + i;
             if (vectorIndex < cols) {
                 myVectorBlock[i] = vector[vectorIndex];
@@ -42,33 +45,34 @@ void parallelProduct(double* matrix, double* vector, double* result, int rows, i
                 myVectorBlock[i] = 0.0;
             }
         }
-    }
 
-    double* myResultBlock = (double*)malloc(rows * sizeof(double));
-    for (int i = 0; i < rows; i++) {
-        myResultBlock[i] = 0.0;
-        for (int j = 0; j < blockSize; j++) {
-            myResultBlock[i] += myMatrixBlock[i * blockSize + j] * myVectorBlock[j];
+        double* myResultBlock = (double*)malloc(rows * sizeof(double));
+        for (i = 0; i < rows; i++) {
+            myResultBlock[i] = 0.0;
+            for (j = 0; j < blockSize; j++) {
+                myResultBlock[i] += myMatrixBlock[i * blockSize + j] * myVectorBlock[j];
+            }
         }
-    }
 
-    int* recvcounts = (int*)malloc(size * sizeof(int));
-    int* displs = (int*)malloc(size * sizeof(int));
-    for (int i = 0; i < size; i++) {
-        int startBlock = i * numBlocks / size;
-        int endBlock = (i + 1) * numBlocks / size;
-        if (i == size - 1) {
-            endBlock = numBlocks;
+        int* recvcounts = (int*)malloc(size * sizeof(int));
+        int* displs = (int*)malloc(size * sizeof(int));
+        for (i = 0; i < size; i++) {
+            int startBlock = i * numBlocks / size;
+            int endBlock = (i + 1) * numBlocks / size;
+            if (i == size - 1) {
+                endBlock = numBlocks;
+            }
+            recvcounts[i] = rows * (endBlock - startBlock);
+            displs[i] = rows * startBlock;
         }
-        recvcounts[i] = rows * (endBlock - startBlock);
-        displs[i] = rows * startBlock;
-    }
 
-    MPI_Gatherv(myResultBlock, rows, MPI_DOUBLE, result, recvcounts, displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Gatherv(myResultBlock, rows, MPI_DOUBLE, result, recvcounts, displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        free(myResultBlock);
+        free(recvcounts);
+        free(displs);
+    }
 
     free(myMatrixBlock);
     free(myVectorBlock);
-    free(myResultBlock);
-    free(recvcounts);
-    free(displs);
 }
